d1a39214d3853de7cd0adf653150c8b29644eda6
Lula Leus <lula@startmonday.com>
2019-09-29 11:23:03 +0100

    Navigation from expenses list to add expense and back

--- app/src/main/java/com/lulius/fifty_thirty_twenty_budget/MainNavigationActivity.kt
+++ app/src/main/java/com/lulius/fifty_thirty_twenty_budget/MainNavigationActivity.kt
@@ -2,11 +2,14 @@ package com.lulius.fifty_thirty_twenty_budget
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.lulius.fifty_thirty_twenty_budget.expenses.ExpensesListFragment
 
 class MainNavigationActivity : AppCompatActivity() {
@@ -32,10 +35,29 @@ class MainNavigationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController);
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.app_bar_actions, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.action_add_expense -> {
+                this.findNavController(R.id.nav_host_fragment).navigate(R.id.addExpense, null)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp();
     }
 }
--- app/src/main/java/com/lulius/fifty_thirty_twenty_budget/expense.kt
+++ app/src/main/java/com/lulius/fifty_thirty_twenty_budget/expense.kt
@@ -1,4 +1,4 @@
-package com.lulius.fifty_thirty_twenty_budget.expenses
+package com.lulius.fifty_thirty_twenty_budget
 
 import android.content.Context
 import android.net.Uri
@@ -8,23 +8,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-import com.lulius.fifty_thirty_twenty_budget.R
-
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
+
 /**
  * A simple [Fragment] subclass.
  * Activities that contain this fragment must implement the
- * [AddExpence.OnFragmentInteractionListener] interface
+ * [ExpenseOverview.OnFragmentInteractionListener] interface
  * to handle interaction events.
- * Use the [AddExpence.newInstance] factory method to
+ * Use the [ExpenseOverview.newInstance] factory method to
  * create an instance of this fragment.
- *
  */
-class AddExpence : Fragment() {
+class ExpenseOverview : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -43,7 +41,7 @@ class AddExpence : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_expence, container, false)
+        return inflater.inflate(R.layout.fragment_expense, container, false)
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -88,12 +86,12 @@ class AddExpence : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment AddExpence.
+         * @return A new instance of fragment expences.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AddExpence().apply {
+            ExpenseOverview().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
